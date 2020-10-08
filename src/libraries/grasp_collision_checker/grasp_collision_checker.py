a75969d0da6be900c148eebf88f67f08c7c49f56
Hanbo Zhang <zhanghanbo163@stu.xjtu.edu.cn>
2020-10-08 21:56:52 +0800

    cuda for acceleration

--- src/libraries/grasp_collision_checker/grasp_collision_checker.py
+++ src/libraries/grasp_collision_checker/grasp_collision_checker.py
@@ -3,7 +3,63 @@ import numpy as np
 import time
 from scipy.spatial.transform import Rotation as R
 import math
+import torch
 
+
+import stl
+import os.path as osp
+import sys
+this_dir = osp.dirname(osp.abspath(__file__))
+sys.path.insert(0, osp.join(this_dir, '../../'))
+from config.config import CLASSES, ROOT_DIR
+
+# ------- Settings ---------
+GRASP_BOX_FOR_SEG = 1
+BBOX_FOR_SEG = 2
+GRASP_BOX_6DOF_PICK = 3
+USE_REALSENSE = True
+ABSOLUTE_COLLISION_SCORE_THRESHOLD = 20
+IN_GRIPPER_SCORE_THRESOHOLD = 40
+VISUALIZE_GRASP = False
+DUMMY_LISTEN = True
+DUMMY_GRASP = False
+
+# ------- Constants ---------
+CONFIG_DIR = osp.join(ROOT_DIR, "config")
+GRIPPER_FILE = "gripper_link.STL"
+LEFT_GRIPPER_FINGER_FILE = "l_gripper_finger_link.STL"
+RIGHT_GRIPPER_FINGER_FILE = "r_gripper_finger_link.STL"
+LEFT_FINGER_POSE = {"link":[0., -0.101425, 0., 0., 0., 0.],
+                    "joint": [0., -0.015425, 0., 0., 0., 0.],
+                    "min_max_translate":[0.0, -0.04]} # - means the direction
+RIGHT_FINGER_POSE = {"link":[0., 0.101425, 0., 0., 0., 0.],
+                     "joint": [0., 0.015425, 0., 0., 0., 0.],
+                     "min_max_translate":[0.0, 0.04]}
+ORIG_IMAGE_SIZE = (480, 640)
+# SCALE = 0.8
+# Y_OFFSET = int(ORIG_IMAGE_SIZE[0] * (1 - SCALE) / 2)
+# X_OFFSET = int(ORIG_IMAGE_SIZE[1] * (1 - SCALE) / 2)
+# YCROP = (Y_OFFSET, ORIG_IMAGE_SIZE[0] - Y_OFFSET)
+# XCROP = (X_OFFSET, ORIG_IMAGE_SIZE[1] - X_OFFSET)
+if USE_REALSENSE:
+    # YCROP = (180, 450)
+    # XCROP = (200, 500)
+    YCROP = (470, 1000)
+    XCROP = (700, 1460)
+else:
+    YCROP = (180, 450)
+    XCROP = (150, 490)
+FETCH_GRIPPER_LENGTH = 0.2
+GRASP_DEPTH = 0.01
+GRASP_POSE_X_OFFST = -0.028
+GRASP_POSE_Y_OFFST = 0.024
+GRASP_POSE_Z_OFFST = -0.017
+GRIPPER_OPENING_OFFSET = 0.01
+GRIPPER_OPENING_MAX = 0.09
+PLACE_BBOX_SIZE = 80
+APPROACH_DIST = 0.1
+RETREAT_DIST = 0.1
+GRASP_BOX_TO_GRIPPER_OPENING = 0.0006
 # ------------- Settings -------------
 ABSOLUTE_COLLISION_SCORE_THRESHOLD = 20
 IN_GRIPPER_SCORE_THRESOHOLD = 40
@@ -87,15 +143,13 @@ class GraspCollisionChecker():
         @param points, np array, [num_grasps, num_points, 3]
         @parma cube, tuple, [[num_grasps, 3], [num_grasps, 3]]
         """
-
         num_grasps = points.shape[0]
         dif_min = points - cube[0].reshape(num_grasps, 1, 3) + epsilon # [num_grasps, num_points, 3]
         dif_max = cube[1].reshape(num_grasps, 1, 3) - points + epsilon # [num_grasps, num_points, 3]
-
         collisions = (((dif_min > 0).sum(-1) == 3) & ((dif_max > 0).sum(-1) == 3)).sum(axis=1) # [num_grasps, 1]
         return collisions
 
-    def _check_grasp_collision(self, scene_pc, grasps):
+    def _check_grasp_collision(self, scene_pc, grasps, use_cuda=False):
         """
         given a 6-d pose of the gripper and the scene point cloud, return whether the grasp is collision-free
         collision-free grasp satisfies:
@@ -106,65 +160,128 @@ class GraspCollisionChecker():
         @param, scene_pc: n_points x 3
         @param, grasps, Nx4
         """
+        if not use_cuda:
+            start_time = time.time()
+            num_grasps = grasps.shape[0]
+            num_points = scene_pc.shape[0]
 
-        start_time = time.time()
+            l_finger_min_orig = self._gripper_model["left_finger"].min_.copy() # [3]
+            l_finger_max_orig = self._gripper_model["left_finger"].max_.copy() # [3]
+            r_finger_min_orig = self._gripper_model["right_finger"].min_.copy() # [3]
+            r_finger_max_orig = self._gripper_model["right_finger"].max_.copy() # [3]
 
-        num_grasps = grasps.shape[0]
-        num_points = scene_pc.shape[0]
+            l_finger_min = np.tile(l_finger_min_orig, (num_grasps, 1)) # num_grasps x 3
+            l_finger_max = np.tile(l_finger_max_orig, (num_grasps, 1)) # num_grasps x 3
+            r_finger_min = np.tile(r_finger_min_orig, (num_grasps, 1)) # num_grasps x 3
+            r_finger_max = np.tile(r_finger_max_orig, (num_grasps, 1)) # num_grasps x 3
 
-        l_finger_min_orig = self._gripper_model["left_finger"].min_.copy() # [3]
-        l_finger_max_orig = self._gripper_model["left_finger"].max_.copy() # [3]
-        r_finger_min_orig = self._gripper_model["right_finger"].min_.copy() # [3]
-        r_finger_max_orig = self._gripper_model["right_finger"].max_.copy() # [3]
+            l_finger_min[:, 1] -= grasps[:, -1] / 2
+            l_finger_min[:, 0] -= 0.031
+            l_finger_max[:, 1] -= grasps[:, -1] / 2
+            l_finger_max[:, 0] -= 0.031
+            r_finger_min[:, 1] += grasps[:, -1] / 2
+            r_finger_min[:, 0] -= 0.031
+            r_finger_max[:, 1] += grasps[:, -1] / 2
+            r_finger_max[:, 0] -= 0.031
 
-        l_finger_min = np.tile(l_finger_min_orig, (num_grasps, 1)) # num_grasps x 3
-        l_finger_max = np.tile(l_finger_max_orig, (num_grasps, 1)) # num_grasps x 3
-        r_finger_min = np.tile(r_finger_min_orig, (num_grasps, 1)) # num_grasps x 3
-        r_finger_max = np.tile(r_finger_max_orig, (num_grasps, 1)) # num_grasps x 3
+            gripper_min = np.minimum(l_finger_min, r_finger_min) # num_grasps x 3
+            gripper_max = np.maximum(l_finger_max, r_finger_max) # num_grasps x 3
+            gripper_min[:, 1] = np.minimum(l_finger_max[:, 1], r_finger_max[:, 1])
+            gripper_max[:, 1] = np.maximum(l_finger_min[:, 1], r_finger_min[:, 1])
 
-        l_finger_min[:, 1] -= grasps[:, -1] / 2
-        l_finger_min[:, 0] -= 0.031
-        l_finger_max[:, 1] -= grasps[:, -1] / 2
-        l_finger_max[:, 0] -= 0.031
-        r_finger_min[:, 1] += grasps[:, -1] / 2
-        r_finger_min[:, 0] -= 0.031
-        r_finger_max[:, 1] += grasps[:, -1] / 2
-        r_finger_max[:, 0] -= 0.031
+            # transfer point to respective grasp coordinates
+            # n_grasp x 4 x 4
+            rot_mats = self._get_rot_mats(grasps)
+            # N x 4
+            scene_pc = np.concatenate([scene_pc, np.ones((scene_pc.shape[0], 1))], axis=1)
+            # n_grasp x 4 x 4 -> 4 x 4 x n_grasp -> 4 x 4*n_grasp
+            rot_mats = np.transpose(rot_mats, (1, 2, 0)).reshape(4, -1)
+            # N x 4*n_grasp
+            scene_pc = np.dot(scene_pc, rot_mats)
+            # N x 4 x n_grasp
+            scene_pc = scene_pc.reshape(scene_pc.shape[0], 4, -1)
+            # n_grasp x N x 3
+            pc_in_g = np.transpose(scene_pc, (2, 0, 1))[:, :, :3]
 
-        gripper_min = np.minimum(l_finger_min, r_finger_min) # num_grasps x 3
-        gripper_max = np.maximum(l_finger_max, r_finger_max) # num_grasps x 3
-        gripper_min[:, 1] = np.minimum(l_finger_max[:, 1], r_finger_max[:, 1])
-        gripper_max[:, 1] = np.maximum(l_finger_min[:, 1], r_finger_min[:, 1])
+            # check collision
+            p_num_collided_l_finger = self._check_collison_for_cube_batch(pc_in_g, (l_finger_min, l_finger_max), epsilon=0) # [num_grasps, 1]
+            p_num_collided_r_finger = self._check_collison_for_cube_batch(pc_in_g, (r_finger_min, r_finger_max), epsilon=0) # [num_grasps, 1]
+            p_num_collided_convex_hull = self._check_collison_for_cube_batch(pc_in_g, (gripper_min, gripper_max), epsilon=-0.01) # [num_grasps, 1]
+            collision_scores = p_num_collided_l_finger + p_num_collided_r_finger # [num_grasps, 1]
+            in_gripper_scores = p_num_collided_convex_hull # - collision_score # [num_grasps, 1]
 
-        # transfer point to respective grasp coordinates
-        # n_grasp x 4 x 4
-        rot_mats = self._get_rot_mats(grasps)
-        # N x 4
-        scene_pc = np.concatenate([scene_pc, np.ones((scene_pc.shape[0], 1))], axis=1)
-        # n_grasp x 4 x 4 -> 4 x 4 x n_grasp -> 4 x 4*n_grasp
-        rot_mats = np.transpose(rot_mats, (1, 2, 0)).reshape(4, -1)
-        # N x 4*n_grasp
-        scene_pc = np.dot(scene_pc, rot_mats)
-        # N x 4 x n_grasp
-        scene_pc = scene_pc.reshape(scene_pc.shape[0], 4, -1)
-        # n_grasp x N x 3
-        pc_in_g = np.transpose(scene_pc, (2, 0, 1))[:, :, :3]
+            valid_mask = in_gripper_scores > 0
+            collision_scores = collision_scores[valid_mask]
+            in_gripper_scores = in_gripper_scores[valid_mask]
+            valid_grasp_inds = np.flatnonzero(valid_mask)
 
-        # check collision
-        p_num_collided_l_finger = self._check_collison_for_cube_batch(pc_in_g, (l_finger_min, l_finger_max), epsilon=0) # [num_grasps, 1]
-        p_num_collided_r_finger = self._check_collison_for_cube_batch(pc_in_g, (r_finger_min, r_finger_max), epsilon=0) # [num_grasps, 1]
-        p_num_collided_convex_hull = self._check_collison_for_cube_batch(pc_in_g, (gripper_min, gripper_max), epsilon=-0.01) # [num_grasps, 1]
-        collision_scores = p_num_collided_l_finger + p_num_collided_r_finger # [num_grasps, 1]
-        in_gripper_scores = p_num_collided_convex_hull # - collision_score # [num_grasps, 1]
+            end_time = time.time()
+            print("_check_grasp_collision takes {}".format(end_time - start_time))
+            return collision_scores, in_gripper_scores, valid_grasp_inds
+        else:
+            start_time = time.time()
+            num_grasps = grasps.shape[0]
+            num_points = scene_pc.shape[0]
+            grasps = torch.FloatTensor(grasps).cuda()
+            scene_pc = torch.FloatTensor(scene_pc).cuda()
 
-        valid_mask = in_gripper_scores > 0
-        collision_scores = collision_scores[valid_mask]
-        in_gripper_scores = in_gripper_scores[valid_mask]
-        valid_grasp_inds = np.flatnonzero(valid_mask)
+            l_finger_min_orig = self._gripper_model["left_finger"].min_.copy()  # [3]
+            l_finger_max_orig = self._gripper_model["left_finger"].max_.copy()  # [3]
+            r_finger_min_orig = self._gripper_model["right_finger"].min_.copy()  # [3]
+            r_finger_max_orig = self._gripper_model["right_finger"].max_.copy()  # [3]
 
-        end_time = time.time()
-        print("_check_grasp_collision takes {}".format(end_time - start_time))
-        return collision_scores, in_gripper_scores, valid_grasp_inds
+            l_finger_min = torch.FloatTensor(np.tile(l_finger_min_orig, (num_grasps, 1))).cuda()  # num_grasps x 3
+            l_finger_max = torch.FloatTensor(np.tile(l_finger_max_orig, (num_grasps, 1))).cuda()  # num_grasps x 3
+            r_finger_min = torch.FloatTensor(np.tile(r_finger_min_orig, (num_grasps, 1))).cuda()  # num_grasps x 3
+            r_finger_max = torch.FloatTensor(np.tile(r_finger_max_orig, (num_grasps, 1))).cuda()  # num_grasps x 3
+
+            l_finger_min[:, 1] -= grasps[:, -1] / 2
+            l_finger_min[:, 0] -= 0.031
+            l_finger_max[:, 1] -= grasps[:, -1] / 2
+            l_finger_max[:, 0] -= 0.031
+            r_finger_min[:, 1] += grasps[:, -1] / 2
+            r_finger_min[:, 0] -= 0.031
+            r_finger_max[:, 1] += grasps[:, -1] / 2
+            r_finger_max[:, 0] -= 0.031
+
+            gripper_min = torch.min(l_finger_min, r_finger_min)  # num_grasps x 3
+            gripper_max = torch.max(l_finger_max, r_finger_max)  # num_grasps x 3
+            gripper_min[:, 1] = torch.min(l_finger_max[:, 1], r_finger_max[:, 1])
+            gripper_max[:, 1] = torch.max(l_finger_min[:, 1], r_finger_min[:, 1])
+
+            # transfer point to respective grasp coordinates
+            # n_grasp x 4 x 4
+            rot_mats = torch.FloatTensor(self._get_rot_mats(grasps.cpu().numpy())).cuda()
+            # N x 4
+            scene_pc = torch.cat([scene_pc, torch.ones((scene_pc.shape[0], 1)).cuda()], dim=1)
+            # n_grasp x 4 x 4 -> 4 x 4 x n_grasp -> 4 x 4*n_grasp
+            rot_mats = rot_mats.permute(1, 2, 0).reshape(4, -1)
+            # N x 4*n_grasp
+            scene_pc = torch.mm(scene_pc, rot_mats)
+            # N x 4 x n_grasp
+            scene_pc = scene_pc.reshape(scene_pc.shape[0], 4, -1)
+            # n_grasp x N x 3
+            pc_in_g = scene_pc.permute(2, 0, 1)[:, :, :3]
+
+            # check collision
+            p_num_collided_l_finger = self._check_collison_for_cube_batch(pc_in_g, (l_finger_min, l_finger_max),
+                                                                          epsilon=0)  # [num_grasps, 1]
+            p_num_collided_r_finger = self._check_collison_for_cube_batch(pc_in_g, (r_finger_min, r_finger_max),
+                                                                          epsilon=0)  # [num_grasps, 1]
+            p_num_collided_convex_hull = self._check_collison_for_cube_batch(pc_in_g, (gripper_min, gripper_max),
+                                                                             epsilon=-0.01)  # [num_grasps, 1]
+            collision_scores = p_num_collided_l_finger + p_num_collided_r_finger  # [num_grasps, 1]
+            in_gripper_scores = p_num_collided_convex_hull  # - collision_score # [num_grasps, 1]
+
+            valid_mask = in_gripper_scores > 0
+            collision_scores = collision_scores[valid_mask]
+            in_gripper_scores = in_gripper_scores[valid_mask]
+            valid_grasp_inds = torch.nonzero(valid_mask.view(-1)).view(-1)
+
+            end_time = time.time()
+            print("_check_grasp_collision takes {}".format(end_time - start_time))
+            return collision_scores.cpu().numpy(), in_gripper_scores.cpu().numpy(), valid_grasp_inds.cpu().numpy()
+
 
     def _select_from_grasps(self, grasps, scene_pc):
         print("_select_from_grasps: num_of_grasps: {}, num_of_points: {}".format(grasps.shape[0], scene_pc.shape[0]))
@@ -276,11 +393,25 @@ class GraspCollisionChecker():
         return new_grasp_dict
 
 if __name__=="__main__":
+    gripper_model_path = osp.join(CONFIG_DIR, GRIPPER_FILE)
+    l_finger_model_path = osp.join(CONFIG_DIR, LEFT_GRIPPER_FINGER_FILE)
+    r_finger_model_path = osp.join(CONFIG_DIR, RIGHT_GRIPPER_FINGER_FILE)
+    gripper_mesh = stl.mesh.Mesh.from_file(gripper_model_path)
+    l_finger_mesh = stl.mesh.Mesh.from_file(l_finger_model_path)
+    r_finger_mesh = stl.mesh.Mesh.from_file(r_finger_model_path)
+    items = 1, 4, 7
+    # since the model only imposes a y axis translate on the two fingers,
+    # we here only consider this translate.
+    l_finger_mesh.points[:, items] += LEFT_FINGER_POSE["link"][1] + LEFT_FINGER_POSE["joint"][1]
+    r_finger_mesh.points[:, items] += RIGHT_FINGER_POSE["link"][1] + RIGHT_FINGER_POSE["joint"][1]
+    gripper = {"gripper": gripper_mesh, "left_finger": l_finger_mesh, "right_finger": r_finger_mesh}
+
     r1 = R.from_euler("zyx", [0, math.pi / 2, 0])
     r2 = R.from_euler("zyx", [0, math.pi / 3, math.pi / 3])
     grasps = np.array([[0, 0, 0] + r1.as_quat().tolist(), [0.1, 0, 0] + r2.as_quat().tolist()])
     grasps = np.tile(grasps, (1000, 1))
 
-    scene_pc = np.random.rand(10000, 3)
-    col_ck = GraspCollisionChecker(None)
-    col_ck._trans_world_points_to_gripper_batch(scene_pc, grasps)
+    scene_pc = np.random.rand(20000, 3)
+    col_ck = GraspCollisionChecker(gripper)
+    print(col_ck._check_grasp_collision(scene_pc, grasps))
+    print(col_ck._check_grasp_collision(scene_pc, grasps, use_cuda=True))
