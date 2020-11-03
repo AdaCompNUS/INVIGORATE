4f4456bdb1f5b15b42c52a7b85714e07775e287a
Hanbo Zhang <zhanghanbo163@stu.xjtu.edu.cn>
2020-11-03 09:45:53 +0800

    minor changes

--- src/libraries/density_estimator/density_estimator.py
+++ src/libraries/density_estimator/density_estimator.py
@@ -79,31 +79,76 @@ class relation_belief(object):
         self.belief = np.array([0.333, 0.333, 0.334])
 
 if __name__=="__main__":
+    # this_dir = osp.dirname(osp.abspath(__file__))
+    # with open(osp.join(this_dir, '../../invigorate/density_esti_train_data.pkl')) as f:
+    #     data = pkl.load(f)
+    # data = data["ground"]
+    #
+    # pos_data = []
+    # neg_data = []
+    # for d in data:
+    #     for i, score in enumerate(d["scores"]):
+    #         if str(i) in d["gt"]:
+    #             pos_data.append(score)
+    #         else:
+    #             neg_data.append(score)
+    # pos_data = np.expand_dims(np.array(pos_data), axis=-1)
+    # pos_data = np.sort(pos_data, axis=0)[5:-5]
+    # neg_data = np.expand_dims(np.array(neg_data), axis=-1)
+    # neg_data = np.sort(neg_data, axis=0)[5:-5]
+    #
+    # kde_pos = gaussian_kde(pos_data)
+    # kde_neg = gaussian_kde(neg_data)
+    #
+    # x = (np.arange(100).astype(np.float32) / 100 - 0.5) * 2
+    # y = np.array([kde_pos.comp_prob(x[i]) for i in range(len(x))])
+    # plt.plot(x, y, ls="-", lw=2, label="positive")
+    # y = np.array([kde_neg.comp_prob(x[i]) for i in range(len(x))])
+    # plt.plot(x, y, ls="-", lw=2, label="negative")
+    # plt.xlabel("Grounding score")
+    # plt.ylabel("Density")
+    # plt.legend()
+    # plt.show()
+
     this_dir = osp.dirname(osp.abspath(__file__))
-    with open(osp.join(this_dir, 'density_esti_train_data.pkl')) as f:
+    # with open(osp.join(this_dir, '../../scripts/density_esti_train_data.pkl')) as f:
+    with open(osp.join(this_dir, '../../scripts/rel_nus.pkl')) as f:
         data = pkl.load(f)
-    data = data["ground"]
 
-    pos_data = []
-    neg_data = []
-    for d in data:
-        for i, score in enumerate(d["scores"]):
-            if str(i) in d["gt"]:
-                pos_data.append(score)
-            else:
-                neg_data.append(score)
-    pos_data = np.expand_dims(np.array(pos_data), axis=-1)
-    pos_data = np.sort(pos_data, axis=0)[5:-5]
-    neg_data = np.expand_dims(np.array(neg_data), axis=-1)
-    neg_data = np.sort(neg_data, axis=0)[5:-5]
+    parents = []
+    childs = []
+    norels = []
+    for i, d in enumerate(data["gt_mrts"]):
+        num_rels = d.shape[1] * d.shape[2]
+        d = d.reshape((3, -1))
+        det = data["det_mrts"][i].reshape((3, -1))
+        for n_rel in range(num_rels):
+            r = d[:, n_rel]
+            if r[0] == 1:
+                parents.append(det[:, n_rel])
+            elif r[1] == 1:
+                childs.append(det[:, n_rel])
+            elif r[2] == 1:
+                norels.append(det[:, n_rel])
 
-    kde_pos = gaussian_kde(pos_data)
-    kde_neg = gaussian_kde(neg_data)
+    parents = np.array(parents)[:, :1]
+    childs = np.array(childs)[:, 1:2]
+    rels = childs + parents
+    rels = np.array(rels)[:, :2].sum(axis = -1)
+    rels = np.expand_dims(rels, axis = -1)
+    norels = 1 - np.array(norels)[:, 2:]
 
-    x = (np.arange(100).astype(np.float32) / 100 - 0.5) * 2
-    y = np.array([kde_pos.comp_prob(x[i]) for i in range(len(x))])
-    plt.plot(x, y, ls="-", lw=2, label="positive")
-    y = np.array([kde_neg.comp_prob(x[i]) for i in range(len(x))])
-    plt.plot(x, y, ls="-", lw=2, label="negative")
+    kde_p = gaussian_kde(parents, bandwidth=0.05)
+    kde_c = gaussian_kde(childs, bandwidth=0.05)
+    kde_r = gaussian_kde(rels, bandwidth=0.05)
+    kde_n = gaussian_kde(norels, bandwidth=0.05)
+
+    x = (np.arange(100).astype(np.float32) / 100)
+    y = np.array([kde_r.comp_prob(x[i]) for i in range(len(x))])
+    plt.plot(x, y, ls="-", lw=2, label="rel")
+    y = np.array([kde_n.comp_prob(x[i]) for i in range(len(x))])
+    plt.plot(x, y, ls="-", lw=2, label="no_rel")
+    plt.xlabel("Rel confidence score")
+    plt.ylabel("Density")
     plt.legend()
     plt.show()
