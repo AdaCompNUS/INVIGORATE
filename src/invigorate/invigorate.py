cb4adc34d482de4e617db40ad91835e4ea79c794
Hanbo Zhang <zhanghanbo163@stu.xjtu.edu.cn>
2021-01-30 19:11:43 +0800

    fix a bug

--- src/invigorate/invigorate.py
+++ src/invigorate/invigorate.py
@@ -626,6 +626,9 @@ class Invigorate(object):
         for k, v in det_to_pool.items():
             self.object_pool[v]["bbox"] = bboxes[k]
             self.object_pool[v]["cls_scores"].append(scores[k].tolist())
+        for i in range(len(self.object_pool)):
+            if i not in det_to_pool.values():
+                self.object_pool[i]["removed"] = True
         # initialize newly detected bboxes
         for i in not_matched:
             new_box = self._init_object(bboxes[i], scores[i])
