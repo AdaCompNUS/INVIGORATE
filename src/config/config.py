9e152074762010c674a1978a5bf6fb40578312f5
lyf44 <luyunfan44@gmail.com>
2021-02-09 11:18:19 +0800

    Clean config

--- src/config/config.py
+++ src/config/config.py
@@ -53,10 +53,6 @@ ASK_Q1 = 2
 #                'badminton', 'wallet', 'wrist developer', 'glasses', 'plier', 'headset',
 #                'toothbrush', 'card', 'paper', 'towel', 'shaver', 'watch']
 
-CLASSES_2 = ['__background__',  # always index 0
-               'box', 'banana', 'notebook', 'apple', 'mobile phone', 'bottle', 'pen', 'mouse', 'umbrella',
-               'remote controller', 'can', 'knife', 'cup', 'wallet', 'glasses', 'toothbrush']
-
 CLASSES = [
 '__background__',
 'sports ball' ,
@@ -76,104 +72,21 @@ CLASSES = [
 'box' ,
 ]
 
-CLASSES_2 = ['__background__',  # always index 0
-               'box', 'banana', 'notebook', 'apple', 'mobile phone', 'bottle', 'pen', 'mouse', 'umbrella',
-               'remote controller', 'can', 'knife', 'cup', 'wallet', 'glasses', 'toothbrush']
-
-COCO_CLASSES = [u'__background__', u'person', u'bicycle', u'car',
-u'motorcycle',
-u'airplane',
-u'bus',
-u'train',
-u'truck',
-u'boat',
-u'traffic light',
-u'fire hydrant',
-u'stop sign',
-u'parking meter',
-u'bench',
-u'bird',
-u'cat',
-u'dog',
-u'horse',
-u'sheep',
-u'cow',
-u'elephant',
-u'bear',
-u'zebra',
-u'giraffe',
-u'backpack',
-u'umbrella',
-u'handbag',
-u'tie',
-u'suitcase',
-u'frisbee',
-u'skis',
-u'snowboard',
-u'sports ball',
-u'kite',
-u'baseball bat',
-u'baseball glove',
-u'skateboard',
-u'surfboard',
-u'tennis racket',
-u'bottle',
-u'wine glass',
-u'cup',
-u'fork',
-u'knife',
-u'spoon',
-u'bowl',
-u'banana',
-u'apple',
-u'sandwich',
-u'orange',
-u'broccoli',
-u'carrot',
-u'hot dog',
-u'pizza',
-u'donut',
-u'cake',
-u'chair',
-u'couch',
-u'potted plant',
-u'bed',
-u'dining table',
-u'toilet',
-u'tv',
-u'laptop',
-u'mouse',
-u'remote',
-u'keyboard',
-u'cell phone',
-u'microwave',
-u'oven',
-u'toaster',
-u'sink',
-u'refrigerator',
-u'book',
-u'clock',
-u'vase',
-u'scissors',
-u'teddy bear',
-u'hair drier',
-u'toothbrush']
-
 CLASSES_TO_IND = dict(zip(CLASSES, range(len(CLASSES))))
 
 TEST = 0
 EXPERIMENT = 1
 
 # --------------- Settings ------------------
-MODE = TEST # 0 for test, 1 for experiment
+MODE = EXPERIMENT # 0 for test, 1 for experiment
 # EXP_SETTING = "baseline" # choose from: baseline, no_uncert, no_multistep, invigorate
 # EXP_SETTING = "greedy" # choose from: baseline, no_uncert, no_multistep, invigorate
-# EXP_SETTING = "heuristic" # choose from: baseline, no_uncert, no_multistep, invigorate
-EXP_SETTING = "invigorate" # choose from: baseline, no_uncert, no_multistep, invigorate
+EXP_SETTING = "heuristic" # choose from: baseline, no_uncert, no_multistep, invigorate
+# EXP_SETTING = "invigorate" # choose from: baseline, no_uncert, no_multistep, invigorate
 
 # ------------- EXP Settings --------------
-PARTICIPANT_NUM = 1
-SCENE_NUM = 6
+PARTICIPANT_NUM = 4
+SCENE_NUM = 10
 VER_NUM = 0
 if EXP_SETTING == "baseline":
     VER_NUM = 1
