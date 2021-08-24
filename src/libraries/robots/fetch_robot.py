94630c515fe3f6329ebfa85fdc46ec69f31edae1
lyf44 <luyunfan44@gmail.com>
2021-08-24 17:39:24 +0800

    Bug fix for Fetch Demo<2>

--- src/libraries/robots/fetch_robot.py
+++ src/libraries/robots/fetch_robot.py
@@ -68,12 +68,12 @@ FETCH_YCROP = (180, 450) # 480
 FETCH_XCROP = (150, 490) # 640
 
 FETCH_GRIPPER_LENGTH = 0.2
-FETCH_MAX_GRIPPER_OPENING = 0.1
-GRASP_DEPTH = 0.01
+FETCH_MAX_GRIPPER_OPENING = 0.07
+GRASP_DEPTH = 0.008
 INITIAL_GRASP_Z_OFFSET = 0.0
-GRASP_POSE_X_OFFST = -0.00 # -0.018
-GRASP_POSE_Y_OFFST = 0.000 # 0.02
-GRASP_POSE_Z_OFFST = -0.001 # -0.015
+GRASP_POSE_X_OFFST = -0.015 # -0.018
+GRASP_POSE_Y_OFFST = 0.00 # 0.02
+GRASP_POSE_Z_OFFST = -0.008 # -0.015
 GRASP_WIDTH_OFFSET = 0.0
 GRIPPER_OPENING_OFFSET = 0.01
 GRIPPER_OPENING_MAX = 0.09
@@ -105,16 +105,16 @@ class FetchRobot():
         self._head = fetch_api.Head()
         self._gripper = fetch_api.Gripper()
 
-        #
-        self._torso.set_height(0.15)
-        self._head.pan_tilt(pan=0, tilt= math.radians(55), duration=2)
-
         # call pnp service to get ready
         pnp_req = PickPlaceRequest()
         pnp_req.action = PickPlaceRequest.GET_READY
+        pnp_req.torso_height = 0.05
         resp = self._pnp_client(pnp_req)  # get existing result
         if not resp.success:
             raise RuntimeError('fetch failed to get ready for pick n place!!!')
+
+        self._torso.set_height(0.15)
+        self._head.pan_tilt(pan=0, tilt= math.radians(55), duration=2)
 
         pnp_req = PickPlaceRequest()
         pnp_req.action = PickPlaceRequest.MOVE_ARM_TO_HOME
@@ -190,12 +190,13 @@ class FetchRobot():
         grasp_box = grasp_box + np.tile([XCROP[0], YCROP[0]], 4)
         # for grasp box, x1,y1 is topleft, x2,y2 is topright, x3,y3 is btmright, x4,y4 is btmleft
         x1, y1, x2, y2, x3, y3, x4, y4 = grasp_box.tolist()
+        angle = math.atan2(y2 - y1, x2 - x1)
         seg_req = BBoxSegmentationRequest()
         seg_req.x = x1
         seg_req.y = y1
         seg_req.width = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         seg_req.height = math.sqrt((x4 - x1)**2 + (y4 - y1)**2)
-        seg_req.angle = math.atan2(y2 - y1, x2 - x1)
+        seg_req.angle = angle
         seg_req.transform_to_reference_frame = True
         seg_req.reference_frame = 'base_link'
 
@@ -215,14 +216,32 @@ class FetchRobot():
         obj_height = seg_resp.object.primitive.dimensions[SolidPrimitive.BOX_Z]
         print("obj_x, y, z: {} {} {}".format(obj_length, obj_width, obj_height))
 
+        # seg request again to get grasp box center pos
+        grasp_box_cx = x4 + (x2 - x4) / 2
+        grasp_box_cy = y2 + (y4 - y2) / 2
+        seg_req.x = grasp_box_cx - 2
+        seg_req.y = grasp_box_cy - 2
+        seg_req.width = 5
+        seg_req.height = 5
+        seg_req.angle = 0
+        seg_req.transform_to_reference_frame = True
+        seg_req.reference_frame = 'base_link'
+
+        seg_resp = self._bbox_segmentation_client(seg_req)
+        xy_pose = seg_resp.object.primitive_pose
+
+        print("obj_pose_x: {}, obj_pose_y: {}".format(obj_pose.position.x, obj_pose.position.y))
+        print("xy_pose_x: {}, xy_pose_y: {}".format(xy_pose.position.x, xy_pose.position.y))
+
         grasp = Grasp()
         grasp.grasp_pose.header = seg_resp.object.header
-        grasp.grasp_pose.pose = seg_resp.object.primitive_pose
-        grasp.grasp_pose.pose.position.z += obj_height / 2 - GRASP_DEPTH
+        grasp.grasp_pose.pose.position.x = xy_pose.position.x
+        grasp.grasp_pose.pose.position.y = xy_pose.position.y
+        grasp.grasp_pose.pose.position.z = obj_pose.position.z + obj_height / 2 - GRASP_DEPTH
         # grasp.grasp_pose.pose.position.z -= GRASP_DEPTH
         # grasp.grasp_pose.pose.position.z += INITIAL_GRASP_Z_OFFSET
-        quat = T.quaternion_from_euler(0, math.pi / 2, seg_req.angle, 'rzyx') # rotate by y to make it facing downwards
-                                                                              # rotate by z to align with bbox orientation
+        quat = T.quaternion_from_euler(0, math.pi / 2, angle, 'rzyx') # rotate by y to make it facing downwards
+                                                                      # rotate by z to align with bbox orientation
         grasp.grasp_pose.pose.orientation.x = quat[0]
         grasp.grasp_pose.pose.orientation.y = quat[1]
         grasp.grasp_pose.pose.orientation.z = quat[2]
@@ -252,25 +271,34 @@ class FetchRobot():
         pnp_req.grasp = grasp
         pnp_req.gripper_opening = gripper_opening
 
-        grasp_pose_tmp = grasp.grasp_pose
-        new_grasp = self._get_collision_free_grasp(grasp_pose_tmp, pnp_req.gripper_opening)
-        # to_cont = raw_input("to_continue?")
-        # if to_cont != "y":
+        # grasp_pose_tmp = grasp.grasp_pose
+        # new_grasp = self._get_collision_free_grasp(grasp_pose_tmp, pnp_req.gripper_opening)
+        # # to_cont = raw_input("to_continue?")
+        # # if to_cont != "y":
+        # #     return False
+
+        # if new_grasp is None:
+        #     logger.error('ERROR: robot grasp failed!!')
         #     return False
 
-        if new_grasp is None:
-            logger.error('ERROR: robot grasp failed!!')
-            return False
+        # grasp.grasp_pose.pose.position.x = new_grasp["pos"][0]
+        # grasp.grasp_pose.pose.position.y = new_grasp["pos"][1]
+        # grasp.grasp_pose.pose.position.z = new_grasp["pos"][2]
+        # gripper_opening = new_grasp["width"]
 
-        grasp.grasp_pose.pose.position.x = new_grasp["pos"][0]
-        grasp.grasp_pose.pose.position.y = new_grasp["pos"][1]
-        grasp.grasp_pose.pose.position.z = new_grasp["pos"][2]
         grasp.grasp_pose.pose.position.x += GRASP_POSE_X_OFFST # HACK!!!
         grasp.grasp_pose.pose.position.y += GRASP_POSE_Y_OFFST # HACK!!!
         grasp.grasp_pose.pose.position.z += APPROACH_DIST + FETCH_GRIPPER_LENGTH + GRASP_POSE_Z_OFFST #HACK!!!
+        gripper_opening += GRASP_WIDTH_OFFSET # HACK!!
+
+        print(grasp)
 
         pnp_req.grasp = grasp
-        pnp_req.gripper_opening = new_grasp["width"] + GRASP_WIDTH_OFFSET # HACK!!!
+        pnp_req.gripper_opening = gripper_opening
+
+        to_cont = raw_input("to_continue?")
+        if to_cont == "n":
+            return False
 
         resp = self._pnp_client(pnp_req)
         num_attempt = 1
@@ -387,7 +415,7 @@ class FetchRobot():
             return False
 
         if not is_target:
-            self.place_object(target_pose)
+            # self.place_object(target_pose)
             self.move_arm_to_home()
         else:
             self.give_obj_to_human()
